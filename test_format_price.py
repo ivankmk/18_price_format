d17572e34a9071070a01e9c5b56687af6af4c8bd
ivankmk <kumeyko@hotmail.com>
2018-04-02 20:19:34 +0200

    script amended

--- test_format_price.py
+++ test_format_price.py
@@ -3,20 +3,26 @@ from format_price import format_price
 
 
 class TestFormatPrice(unittest.TestCase):
+
     def test_big_number(self):
-        self.assertEqual(format_price(123456789123), '123 456 789 123')
+        test_value1 = 123456789123
+        self.assertEqual(format_price(test_value1), '123 456 789 123')
 
     def test_decimals(self):
-        self.assertEqual(format_price(123456.123), '123 456.123')
+        test_value2 = 123456.123
+        self.assertEqual(format_price(test_value2), '123 456.123')
 
     def test_zero_decimals(self):
-        self.assertEqual(format_price(123456.0), '123 456')
+        test_value3 = 123456.0
+        self.assertEqual(format_price(test_value3), '123 456')
 
     def test_minus(self):
-        self.assertEqual(format_price(-123456), '-123 456')
+        test_value4 = -123456
+        self.assertEqual(format_price(test_value4), '-123 456')
 
     def test_string_arg(self):
-        self.assertEqual(format_price('string'), None)
+        test_value5 = 'string'
+        self.assertEqual(format_price(test_value5), None)
 
 
 if __name__ == '__main__':
