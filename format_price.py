c740967a851b1bb5b5583778ac6cc9a009fcd8d2
ivankmk <kumeyko@hotmail.com>
2018-04-07 21:54:08 +0200

    script amended

--- format_price.py
+++ format_price.py
@@ -1,28 +1,23 @@
 import sys
-import re
 
 
 def format_price(price):
-    zero_after_dot = re.compile("[-]?[0-9]*[.]?[0]*\Z")
-    one_dot = re.compile("[-]?[0-9]*[.]?[0-9]*\Z")
-    price = str(price)
-
-    if bool(one_dot.match(price)):
-        if bool(zero_after_dot.match(price)):
+    try:
+        price = float(price)
+        if price.is_integer():
             return '{0:,}'.format(round(float(price))).replace(',', ' ')
         else:
             return '{0:,}'.format(float(price)).replace(',', ' ')
-    else:
+    except ValueError:
         return None
 
 
 if __name__ == '__main__':
     try:
         price = sys.argv[1]
-        if format_price(price) is not None:
-            print(format_price(price))
-        else:
-            print('Please, use only numbers.')
-
     except IndexError:
         print('Please, insert the number to apply formatting.')
+    if format_price(price) is not None:
+        print(format_price(price))
+    else:
+        print('Please, use only numbers.')
--- tests.py
+++ tests.py
@@ -12,17 +12,37 @@ class TestFormatPrice(unittest.TestCase):
         test_value2 = 123456.123
         self.assertEqual(format_price(test_value2), '123 456.123')
 
+    def test_big_decimal(self):
+        test_value3 = 123456.123123123
+        self.assertEqual(format_price(test_value3), '123 456.123123123')
+
     def test_zero_decimals(self):
-        test_value3 = 123456.0
-        self.assertEqual(format_price(test_value3), '123 456')
+        test_value4 = 123456.0
+        self.assertEqual(format_price(test_value4), '123 456')
+
+    def test_a_lot_of_zeros(self):
+        test_value5 = 123456.00000000000000
+        self.assertEqual(format_price(test_value5), '123 456')
+
+    def test_a_lot_of_zeros_and_negative(self):
+        test_value6 = -123456.00000000000000
+        self.assertEqual(format_price(test_value6), '-123 456')
 
     def test_minus(self):
-        test_value4 = -123456
-        self.assertEqual(format_price(test_value4), '-123 456')
+        test_value7 = -123456
+        self.assertEqual(format_price(test_value7), '-123 456')
+
+    def test_minus_and_float(self):
+        test_value8 = -123456.888
+        self.assertEqual(format_price(test_value8), '-123 456.888')
 
     def test_string_arg(self):
-        test_value5 = 'string'
-        self.assertEqual(format_price(test_value5), None)
+        test_value9 = 'string'
+        self.assertEqual(format_price(test_value9), None)
+
+    def test_mixed_string_arg(self):
+        test_value10 = '123412string'
+        self.assertEqual(format_price(test_value10), None)
 
 
 if __name__ == '__main__':
